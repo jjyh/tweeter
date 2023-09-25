e2c02660b1fa17f655be93231a87d73c1a1e3a0b
jjyh <janejyh@gmail.com>
2023-09-24 21:40:00 -0400

    add error messages pop up

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -34,15 +34,24 @@ $(document).ready(function() {
 
   $("#new-tweet-form").submit(function(event) {
     event.preventDefault();
-    const newTweet = $(this).serialize();
-    $.post("/tweets/", newTweet);
+
+    const inputLength = $(this).find("#tweet-text").val().length;    
+    if (!inputLength) {
+      return alert("Please enter text to tweet");
+    } else if (inputLength > 140) {
+      return alert("Please shorten the tweet to within 140 character");
+    } else {
+      const newTweet = $(this).serialize();
+      $.post("/tweets/", newTweet);
+    }
   });
 
-  
+   //appending tweets to #tweet-container for render 
   const renderTweets = function(tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
+    //$('#tweets-container').empty();
     for (let tweet of tweets) {
       const $tweet = createTweetElement(tweet);
       //console.log($tweet);
@@ -50,41 +59,45 @@ $(document).ready(function() {
     }
   };
 
+  //create the tweet html from the data object
   const createTweetElement = function(tweet) {
   /* Your code for creating the tweet element */
     let $tweet = $(`
-    <article class="tweet">
-          <header class="tweet-header">
-            <div class="name-left">
-              <img class="user-icon" src="${tweet.user.avatars}"></img> 
-              <h3>${tweet.user.name}</h3>
-            </div>
-            <div class="name-right">
-              ${tweet.user.handle}
-            </div>
-          </header>
-          <div class="posted-tweet">
-            ${tweet.content.text}
+      <article class="tweet">
+        <header class="tweet-header">
+          <div class="name-left">
+            <img class="user-icon" src="${tweet.user.avatars}"></img> 
+            <h3>${tweet.user.name}</h3>
           </div>
-          <footer>
-            <span class=""footer-left"">${timeago.format(tweet.created_at)}</span>
-            <div class="footer-right">
-              <div class='icons'>
-                <i class="fas fa-flag"></i>
-                <i class="fas fa-retweet"></i>
-                <i class="fas fa-heart"></i>
-              </div>
+          <div class="name-right">
+            ${tweet.user.handle}
+          </div>
+        </header>
+        <div class="posted-tweet">
+          ${tweet.content.text}
+        </div>
+        <footer>
+          <span class=""footer-left"">${timeago.format(tweet.created_at)}</span>
+          <div class="footer-right">
+            <div class='icons'>
+              <i class="fas fa-flag"></i>
+              <i class="fas fa-retweet"></i>
+              <i class="fas fa-heart"></i>
             </div>
-          </footer>
-        </article>`);
+          </div>
+        </footer>
+      </article>`);
     return $tweet;
   };
 
+  //GET tweets from server sort
   const loadTweets = function() {
     $.get("/tweets/", function(newTweet) {
       renderTweets(newTweet);
-    }); 
+    });
   };
 
   loadTweets();
+
+  
 });
